af9b0aca9638b7119655f695e74a69d068de543c
Andriiprog08 <andriipalyvoda674@gmail.com>
2025-10-20 22:30:23 +0200

    footer_end_menu_delet_cz_app_new_lists

--- app.py
+++ app.py
@@ -2,9 +2,59 @@ from flask import  Flask, render_template
 
 app = Flask (__name__)
 
+tattoos = {
+    1:{
+        "name": "Blackworks",
+        "link": "#",
+    },
+    2:{
+        "name": "Irezumi",
+        "link": "#",
+    },
+    3:{
+        "name": "Old school",
+        "link": "#",
+    },
+    4:{
+        "name": "Dotwork",
+        "link": "#",
+    },
+    5:{
+        "name": "Polynesian",
+        "link": "#",
+    },
+    6:{
+        "name": "Philosophical",
+        "link": "#",
+    },
+}
+
+products = {
+    1:{
+        "name": "Paints and needles",
+        "link": "#",
+    },
+    2:{
+        "name": "Tattoo machines",
+        "link": "#",
+    },
+    3:{
+        "name": "Gel and transfer paper",
+        "link": "#",
+    },
+    4:{
+        "name": "Caps, gloves and other items",
+        "link": "#",
+    },
+    5:{
+        "name": "Shop products",
+        "link": "#",
+    },
+}
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", tattoos = tattoos, products = products)
 
 
 if __name__ == "__main__":
