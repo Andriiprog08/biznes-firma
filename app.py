4d27081e180446293794089afc4c6099a2f12cf8
Andriiprog08 <andriipalyvoda674@gmail.com>
2025-10-21 21:19:04 +0200

    content_work_about_autors

--- app.py
+++ app.py
@@ -1,56 +1,7 @@
 from flask import  Flask, render_template
+from data import tattoos, products
 
 app = Flask (__name__)
-
-tattoos = {
-    1:{
-        "name": "Blackworks",
-        "link": "#",
-    },
-    2:{
-        "name": "Irezumi",
-        "link": "#",
-    },
-    3:{
-        "name": "Old school",
-        "link": "#",
-    },
-    4:{
-        "name": "Dotwork",
-        "link": "#",
-    },
-    5:{
-        "name": "Polynesian",
-        "link": "#",
-    },
-    6:{
-        "name": "Philosophical",
-        "link": "#",
-    },
-}
-
-products = {
-    1:{
-        "name": "Paints and needles",
-        "link": "#",
-    },
-    2:{
-        "name": "Tattoo machines",
-        "link": "#",
-    },
-    3:{
-        "name": "Gel and transfer paper",
-        "link": "#",
-    },
-    4:{
-        "name": "Caps, gloves and other items",
-        "link": "#",
-    },
-    5:{
-        "name": "Shop products",
-        "link": "#",
-    },
-}
 
 @app.route("/")
 def home():
---
+++ data.py
@@ -0,0 +1,49 @@
+tattoos = {
+    1:{
+        "name": "Blackworks",
+        "link": "#",
+    },
+    2:{
+        "name": "Irezumi",
+        "link": "#",
+    },
+    3:{
+        "name": "Old school",
+        "link": "#",
+    },
+    4:{
+        "name": "Dotwork",
+        "link": "#",
+    },
+    5:{
+        "name": "Polynesian",
+        "link": "#",
+    },
+    6:{
+        "name": "Philosophical",
+        "link": "#",
+    },
+}
+
+products = {
+    1:{
+        "name": "Paints and needles",
+        "link": "#",
+    },
+    2:{
+        "name": "Tattoo machines",
+        "link": "#",
+    },
+    3:{
+        "name": "Gel and transfer paper",
+        "link": "#",
+    },
+    4:{
+        "name": "Caps, gloves and other items",
+        "link": "#",
+    },
+    5:{
+        "name": "Shop products",
+        "link": "#",
+    },
+}
